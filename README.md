4691d0f0f565e80d4b7458b6e5566972f77739a1
LumberjackIncorperated <lumerberjacksincorperated@MacBook-Pro-2.local>
2018-05-02 12:05:56 +1000

    .

---
+++ README.md
@@ -0,0 +1,36 @@
+# Frame Project
+
+This project is about our attempt at cleaning up the java frame process
+
+### Installing
+
+```
+git clone https://github.com/LumberjacksIncorperated/FrameProject.git
+```
+
+### Running
+
+You have 4 options after the project has been installed:
+
+```
+# Option 1 : Compile the project
+cd ../project_scripts
+./compile.sh
+
+# Option 2 : Run the project
+cd ../project_scripts
+./run.sh
+
+# Option 3 : Compile and run the project
+cd ../project_scripts
+./compileAndRun.sh
+
+# Option 4: Update to git and push to Github
+cd ../project_scripts
+./updateGit.sh
+```
+
+
+## Authors
+
+Lumberjacks Incorperated (2018)
---
+++ java_files/DisplayInformationDatabase.java
@@ -0,0 +1,53 @@
+//-----------------------------------------------------------------------------------------------------------------------
+//
+// PROJECT
+// -------
+// "Live Information Display Project"
+//
+// AUTHOR
+// ------
+// Lumberjacks Incorperated (2018)
+//
+//-----------------------------------------------------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------------------------------------------------
+// IMPORTS
+//-----------------------------------------------------------------------------------------------------------------------
+import java.util.LinkedList;
+import java.util.Queue;
+
+//-----------------------------------------------------------------------------------------------------------------------
+// CLASS
+//-----------------------------------------------------------------------------------------------------------------------
+public class DisplayInformationDatabase {
+
+    Queue<String> displayInformationQueue;
+
+	public DisplayInformationDatabase(){
+		this.displayInformationQueue = new LinkedList<>();
+	}
+
+	public void addDisplayInformationEntry(String newDisplayInformationEntry) {
+		this.displayInformationQueue.add(newDisplayInformationEntry);
+	}
+
+	public String getADisplayInformationEntry() {
+		String displayInformationEntry;
+		if(queueIsNotEmpty()) {
+			displayInformationEntry = this.displayInformationQueue.remove();
+		} else {
+			displayInformationEntry = null;
+		}
+		return displayInformationEntry;
+	}
+
+	private boolean queueIsNotEmpty() {
+		boolean queueIsNotEmpty;
+		if(this.displayInformationQueue.peek() != null) {
+			queueIsNotEmpty = true;
+		} else {
+			queueIsNotEmpty = false;
+		}
+		return queueIsNotEmpty;
+	}
+}
